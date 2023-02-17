b1ee060913f0ebd89ed39afd0db0fc577bc25c63
Cedric <cedricoppongampadu@gmail.com>
2023-02-16 17:27:03 -0800

    used betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'print a string using puts not printf'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	(return 0);
 }
